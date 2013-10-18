b4a4fac994dfec67f082acc76a94cd83f8fe467c
Ilija Eftimov <ileeftimov@gmail.com>
2013-10-18 22:42:21 +0200

    Initial commit.   * Add Foundation gem   * Add HAML gem   * Add html2haml gem

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+gem 'unicorn'
+gem 'sqlite3'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'zurb-foundation', '~> 4.0.0'
+gem 'haml-rails'
+gem 'html2haml'
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end
+
+gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hirejuniors::Application.config.secret_key_base = '29fc17ee76c42b448d8beafe05e7631b0446de031bcdf06a7d5182aa08dbcedf82cfadc301916b0dc43ac14663d68dab51153c5bac8c1ab8949f0ef5aac08a8c'
