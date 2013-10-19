7afab8775ca8e08e0f9ad7af869a4758f1f1b943
Ilija Eftimov <ileeftimov@gmail.com>
2013-10-19 16:36:55 +0200

    remove job

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Hirejuniors::Application.routes.draw do
-  resources :jobs
-
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
